904b23ef4c6516b65e7ea308c06af3f8f8027e83
Uendi Reveli <uendireveli@gmail.com>
2025-06-02 11:22:12 +0200

    Init

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var map = L.map("map").setView([41.326518, 19.820487], 16);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+var marker = L.marker([41.326518, 19.820487]).addTo(map);
